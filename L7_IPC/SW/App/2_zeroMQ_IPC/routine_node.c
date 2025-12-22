d2bbe87c657d124e990eb3e171efb68a0ec8f7d1
Milos Subotic <milos.subotic.sm@gmail.com>
2025-12-22 18:26:43 +0100

    Skeleton nodes.

---
+++ L7_IPC/SW/App/2_zeroMQ_IPC/routine_node.c
@@ -0,0 +1,14 @@
+
+
+//TODO Copy code from joy_node.c
+
+int main(void) {
+	// TODO just publish next routine and exit:
+	// 1s STOP
+	// 1s CW
+	// 1s STOP
+	// 2s CCW
+	// 1s STOP
+
+	return 0;
+}
---
+++ L7_IPC/SW/App/2_zeroMQ_IPC/stepper_node.c
@@ -0,0 +1,7 @@
+
+//TODO Copy code from wiper_node.c
+//TODO Same as wiper_node, just control stepper.
+
+int main(void) {
+	return 0;
+}
